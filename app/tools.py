30632ea7e36920bcc79dfda37bcc14e1fac38237
Aitor Pérez <aitor.perez@epfl.ch>
2023-11-23 13:08:15 +0100

    :bug: pad color hex codes when needed

--- app/tools.py
+++ app/tools.py
@@ -199,15 +199,15 @@ def ask_graph(human_input: str) -> dict:
 ################################################################
 
 
-def find_color(query: str) -> str:
-    print("[COLOR]", f"Called find color tool with input `{query}`")
+def find_color(name: str) -> str:
+    print("[COLOR]", f"Called find color tool with input `{name}`")
 
-    md5_hash = hashlib.md5(query.encode('utf-8')).hexdigest()
+    md5_hash = hashlib.md5(name.encode('utf-8')).hexdigest()
     seed = int(md5_hash, 16)
 
     random.seed(seed)
     number = random.randint(0, 256 ** 3 - 1)
-    color_hex_code = str(hex(number))[2:]
+    color_hex_code = str(hex(number))[2:].rjust(6, "0")
 
     print("[COLOR]", f"Found color hex code `{color_hex_code}`")
 
