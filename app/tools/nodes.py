fbefb1da58540911b16b0a900d2785f931766bd7
Aitor Pérez <aitor.perez@epfl.ch>
2024-06-18 15:19:57 +0200

    :art: add timestamps for lecture links of concept nodes

--- app/tools/nodes.py
+++ app/tools/nodes.py
@@ -1,11 +1,17 @@
+import re
+from datetime import timedelta
+
+import pandas as pd
+
 from app.interfaces.es import search
+from app.interfaces.db import db_manager
 
 from app.config import config
 
-################################################################
-# Helper functions                                             #
-################################################################
 
+################################################################
+# Cleaner functions                                            #
+################################################################
 
 def clean_link(link):
     link = {
@@ -49,6 +55,89 @@ def clean_nodes(nodes, node_type):
 
 
 ################################################################
+# Timestamp functions                                          #
+################################################################
+
+def get_timestamps(pairs):
+    columns = ['node_type', 'node_id', 'lecture_id', 'timestamp_s']
+
+    if not pairs:
+        return pd.DataFrame(columns=columns)
+
+    table = 'graph_lectures.Data_N_Object_N_Object_T_CalculatedFields'
+    fields = ['to_object_type', 'to_object_id', 'from_object_id', 'field_value']
+    conditions = {
+        'from_institution_id': 'EPFL',
+        'from_object_type': 'Lecture',
+        'to_institution_id': 'Ont',
+        'field_name': 'primary_timestamp',
+        '(to_object_type, to_object_id, from_object_id)': pairs,
+    }
+    timestamps = pd.DataFrame(db_manager.db.find(table_name=table, fields=fields, conditions=conditions), columns=columns)
+
+    return timestamps
+
+
+def to_snake_case(s):
+    # Replace all non-word characters (everything except numbers and letters) with '_'
+    s = re.sub(r'[^\w\s]', ' ', s)
+    # Remove leading and trailing spaces
+    s = s.strip()
+    # Replace all spaces with '_'
+    s = re.sub(r'\s+', '_', s)
+    # Make lowercase
+    s = s.lower()
+    return s
+
+
+def add_lecture_timestamps(nodes):
+    # Crawl the nodes and store the pairs (concept/category, lecture for which we need the timestamps)
+    pairs = []
+    for node in nodes:
+        # Skip if not a concept/category
+        if node['type'] not in ['Concept', 'Category']:
+            continue
+
+        # If a concept/category, store pair for all lecture links
+        for link in node['links']:
+            # Skip if not a lecture
+            if link['type'] != 'Lecture':
+                continue
+
+            pairs.append((node['type'], node['id'], link['id']))
+
+    # Return if no pairs
+    if not pairs:
+        return nodes
+
+    # Fetch timestamps from the database
+    timestamps = get_timestamps(pairs)
+
+    # Iterate again over concept nodes and lecture links to add timestamp field and update the url
+    for node in nodes:
+        # Skip if not a concept
+        if node['type'] not in ['Concept', 'Category']:
+            continue
+
+        # If a concept, add timestamp field and update the lecture url
+        for link in node['links']:
+            # Skip if not a lecture
+            if link['type'] != 'Lecture':
+                continue
+
+            # Add field with timestamp in format
+            try:
+                timestamp_sec = timestamps.loc[(timestamps['node_type'] == node['type']) & (timestamps['node_id'] == node['id']) & (timestamps['lecture_id'] == link['id']), 'timestamp_s'].iloc[0]
+                timestamp_sec = int(timestamp_sec)
+                td = timedelta(seconds=timestamp_sec)
+                link[f"best_timestamp_for_{to_snake_case(node['name_en'])}"] = str(td)
+                link['url'] += f'?t={timestamp_sec}'
+            except (IndexError, TypeError):
+                pass
+
+    return nodes
+
+################################################################
 # Tool function                                                #
 ################################################################
 
@@ -69,9 +158,12 @@ def search_nodes(query: str, node_type: list | str = None) -> list:
     nodes = clean_nodes(nodes, node_type)
     print('[GRAPH]', f"Kept {len(nodes)} nodes after cleanup with {[len(node['links']) for node in nodes]} links")
 
+    # Add timestamps to lectures wherever needed
+    nodes = add_lecture_timestamps(nodes)
+
     return nodes
 
 
 if __name__ == '__main__':
-    nodes = search_nodes("solar cells", node_type=['Lecture', 'Course'])
+    nodes = search_nodes("reduced jordan form", node_type=['Lecture', 'Course'])
     print(nodes)
