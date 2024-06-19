f510170a7d675dc93b20c52787ef79e36d3750cf
Aitor Pérez <aitor.perez@epfl.ch>
2024-06-19 12:16:54 +0200

    :art: add cleanup of already processed tool results to save tokens on subsequent requests

--- app/agent.py
+++ app/agent.py
@@ -9,6 +9,8 @@ from typing import Annotated, Sequence, TypedDict
 from langchain_core.messages import (
     BaseMessage,
     SystemMessage,
+    HumanMessage,
+    AIMessage,
     ToolMessage,
 )
 from langchain_core.runnables import RunnableConfig
@@ -111,10 +113,53 @@ def create_agent():
 
     ################################################################
 
+    def clean_tool_calls_and_responses(messages):
+        # Start setting flag to mark when we will be done checking
+        all_checked = False
+        while not all_checked:
+            # Assume this is the last pass
+            all_checked = True
+
+            # Iterate over all messages, search first AI Message with tool calls
+            for i in range(len(messages) - 1):
+                if isinstance(messages[i], AIMessage) and messages[i].tool_calls:
+                    tool_call_ids = [tool_call['id'] for tool_call in messages[i].tool_calls]
+                    n_tool_calls = len(tool_call_ids)
+
+                    # Make sure there are enough messages after the AI Message
+                    if i + n_tool_calls <= len(messages) - 1:
+                        tool_messages = messages[i + 1: i + n_tool_calls + 1]
+
+                        # Check all tool messages are indeed Tool Messages, otherwise skip to next AI Message
+                        if not all(map(lambda x: isinstance(x, ToolMessage), tool_messages)):
+                            print(f"[AGENT] Found AI Message with {n_tool_calls} tool calls but not followed by {n_tool_calls} Tool Messages")
+                            continue
+
+                        # The AI Message specifies the list of tool call ids.
+                        # Check that the tool messages have that same set of ids, otherwise skip to next AI Message
+                        if set(tool_call_ids) != set(tool_message.tool_call_id for tool_message in tool_messages):
+                            print(f"[AGENT] Found AI Message with {n_tool_calls} tool calls and {n_tool_calls} subsequent Tool Messages, but their ids do not match")
+                            continue
+
+                        print(f"[AGENT] Deleting tool call(s) and response(s): {[(tool_call['name'], tool_call['args']) for tool_call in messages[i].tool_calls]}")
+
+                        # At this point, the AI Message specifies n tool call ids,
+                        # and the next n messages are Tool Messages whose ids are exactly those.
+                        # We delete all n + 1 messages and start over by requiring another pass
+                        del messages[i: i + n_tool_calls + 1]
+                        all_checked = False
+                        break
+
+        return messages
+
     # Define the function that will run in the 'agent' node
     def call_model(state: State, config: RunnableConfig):
         messages = state['messages']
         response = model.invoke(messages, config)
+
+        # Clean up the tool requests and responses if there are no more tool calls
+        if not response.tool_calls:
+            state['messages'] = clean_tool_calls_and_responses(messages)
 
         return {'messages': [response]}
 
@@ -205,14 +250,14 @@ def send_message(conversation_id: str, prompt: str) -> dict:
         returned nodes if applicable, respectively.
     """
 
-    print("[WRAPPER]", f"Received chat request for conversation `{conversation_id}` with input `{prompt}`")
+    print("[AGENT]", f"Received chat request for conversation `{conversation_id}` with input `{prompt}`")
 
     # Reset tools results
     clear_results(conversation_id)
 
     # Invoke model with given prompt and conversation_id
     agent_output = agent.invoke(
-        input={'messages': [('human', prompt)]},
+        input={'messages': [HumanMessage(content=prompt)]},
         config={'configurable': {'thread_id': conversation_id}},
         debug=False
     )
@@ -223,13 +268,13 @@ def send_message(conversation_id: str, prompt: str) -> dict:
     # Log the response message
     display_message = message.replace('\n', ' ')
     if len(display_message) <= 100:
-        print("[WRAPPER]", f"Got response message `{display_message}` from agent executor")
+        print("[AGENT]", f"Got response message `{display_message}` from agent executor")
     else:
-        print("[WRAPPER]", f"Got response message `{display_message[:100]}...` from agent executor")
+        print("[AGENT]", f"Got response message `{display_message[:100]}...` from agent executor")
 
     # Fetch results obtained in the tools
     results = get_results(conversation_id)
-    print("[WRAPPER]", f"Found {len(results)} results from the tools")
+    print("[AGENT]", f"Found {len(results)} results from the tools")
 
     return {
         'message': message,
