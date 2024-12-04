7794a9d70145574ae9c47bfa95d795e064215d4a
Aitor Pérez <aitor.perez@epfl.ch>
2024-12-04 15:08:11 +0100

    :fire: delete deprecated stuff

--- app/deprecated/tools/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.deprecated.tools.color import find_color
-from app.deprecated.tools.exoset import search_exercises
-from app.deprecated.tools.graph import ask_graph
-from app.deprecated.tools.news import search_news
