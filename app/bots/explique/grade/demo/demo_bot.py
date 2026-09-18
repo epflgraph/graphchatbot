8b799b1f955331035f49ccc8f6ae1f958ee06ce8
Spiros Chalkias <sp.chalkias@gmail.com>
2026-09-18 09:34:21 +0200

    test: changed course_id in DemoGradeBot for testing

--- app/bots/explique/grade/demo/demo_bot.py
+++ app/bots/explique/grade/demo/demo_bot.py
@@ -4,5 +4,5 @@ from app.bots.explique.grade.explique_bot import ExpliqueGradeBot
 class DemoGradeBot(ExpliqueGradeBot):
     name = "Explique"
     index = "course_swissunidemo"
-    course_id = 19162
+    course_id = 16234 # 19162
     groups = ["graph-chatbot-admins", "graph-rag-vip", "explique-admins"]
